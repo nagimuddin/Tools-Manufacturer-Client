65942fae8fd064dca8f319a4df5562b0261b9f9e
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-28 16:21:05 +0600

    Products update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Bennar from './Bennar';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div>
             <Bennar></Bennar>
+            <Products></Products>
         </div>
     );
 };
