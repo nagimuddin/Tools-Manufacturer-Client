91bfab129d3126e114ad1ea0bb2475b02c9fa03c
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-28 17:00:51 +0600

    pricing section applied

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Bennar from './Bennar';
+import Pricing from './Pricing';
 import Products from './Products';
+import Steps from './Steps';
 
 const Home = () => {
     return (
         <div>
             <Bennar></Bennar>
             <Products></Products>
+            <Steps></Steps>
+            <Pricing></Pricing>
         </div>
     );
 };
--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -9,134 +9,92 @@ const Products = () => {
             Our Products
           </h1>
         </div>
-        <div className="flex flex-wrap -m-4">
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src="https://dummyimage.com/600x360"
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2>
-                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Shooting Stars
-                </h1>
-                <p className="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src="https://dummyimage.com/601x361"
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2>
-                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  The Catalyzer
-                </h1>
-                <p className="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src="https://dummyimage.com/603x363"
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2>
-                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  The 400 Blows
-                </h1>
-                <p className="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src="https://dummyimage.com/602x362"
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2>
-                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Neptune
-                </h1>
-                <p className="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src="https://dummyimage.com/602x362"
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                  THE SUBTITLE
-                </h2>
-                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                  Neptune
-                </h1>
-                <p className="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-              </div>
-            </div>
-          </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/606x366"
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Alper Kamu
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
         </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p className="mt-1">$21.15</p>
         </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p className="mt-1">$21.15</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268"/>
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
       </div>
     </section>
   );
