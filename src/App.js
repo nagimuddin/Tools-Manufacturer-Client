2784e4cd15ce32d07d6a04b1e400256583c4a6fa
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-05-28 14:30:20 +0600

    set rout and create pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/Login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
